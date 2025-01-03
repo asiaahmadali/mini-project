f18668ee112be4784c6cf0c5246104e7ede1acfe
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-03 19:15:00 +0500

    user model

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express') ;
 const app = express() ;
+const userModel = require('./models/user') ;
+const cookieParser = require('cookie-parser');
+
+app.use(express.json()) ;
+app.use(express.urlencoded({extended:true})) ;
+app.set('view engine','ejs') ;
+app.use(cookieParser());
+
 
 app.get('/',(req,res)=>{
-    res.send('okkk') ;
+    res.render('index')
 })
+
+
 
 app.listen(3000,()=>{
     console.log('server started') ;
