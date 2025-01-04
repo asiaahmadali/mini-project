e6ab59482872bb361c93dfb20889a8d8e118539d
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-04 23:41:10 +0500

    user post

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.get('/edit/id',isLoggedIn, async (req,res)=>{
     
 })
 
-// create post
+// create the post
 
 app.post('/post',isLoggedIn , async (req,res)=>{
     const user = await userModel.findOne({email:req.user.email})
