17e00e18c576ad44a6bba78674b7b20e7f8753c4
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-06 22:36:20 +0500

    post editted

--- app.js
+++ app.js
@@ -125,7 +125,21 @@ app.get('/like/:id',isLoggedIn , async(req, res)=>{
      }else{
        post.likes.splice(post.likes.indexOf(req.user.userid),1)
      }
+     await post.save() ;
      res.redirect('/profile') ;
+})
+
+// edit post
+
+app.get('/edit/:id',isLoggedIn,async (req,res)=>{
+        let post = await postModel.findOne({_id:req.params.id}).populate('user');
+        res.render('edit',{post})
+    })
+
+
+app.post('/update/:id',isLoggedIn ,async (req, res)=>{
+    let post = await postModel.findOneAndUpdate({_id:req.params.id},{content:req.body.content});
+    res.redirect('/profile') ;
 })
 // litsen
 
